8c44ae9689a492daaaaf3c50e2b659a0682ad4f2
Axel-Segovia19 <14segovia@gmail.com>
2022-12-30 21:53:22 -0700

    moved list into a reusable component for this todo list. this way html isnt repeated for the same styled list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,63 +1,22 @@
 <template>
-  <div class="border rounded-lg w-1/4 m-auto mt-2">
+  <div class="border rounded-lg w-1/4 m-auto mt-2 shadow-xl">
     <form class="flex flex-col gap-2 p-3 items-center ">
-      <div class="flex gap-2">
-      <label for="title">Title:</label>
-    <input class="grow" v-model="listItem.title" id="title"/>
-      </div>
-      <div class="flex gap-2" >
-    <label for="todo">To do: </label>
-    <textarea class="" v-model="listItem.todo" id="todo"/>
-      </div>
+      <label for="title" class="self-start">Title:</label>
+    <input class="w-full" v-model="listItem.title" id="title"/>
+    <label for="todo" class="self-start">To do: </label>
+    <textarea class="w-full" v-model="listItem.todo" id="todo"/>
       <button class="bg-cyan-500 text-white p-1 self-end border rounded-lg font-bold" @click.prevent="addToList">Click Me</button>
     </form>
   </div>
-
-
- 
+  
 <div class="flex">
-
     <div class="w-full m-2">
       <h1 class="bg-cyan-500 text-center rounded-t-lg">Not Done</h1>
-    <div v-for="(item, index) in notDoneTodos" :key="item.id" class="flex flex-row border rounded-lg inline float-left m-1">
-      <button class="p-1 border-r bg-cyan-500 rounded-l-lg font-semibold" @click.prevent="remove(index)">X</button>
-      <ul class="p-2">
-      <li class="flex flex-col">
-        <div class="flex gap-1">
-        <label for="title-li" class="font-bold">Title:</label>
-         <p id="title-li">{{item.title}} </p>
-        </div>
-        <div class="flex gap-1">
-         <label for="todo-li" class="font-bold">Todo:</label>
-        <p id="todo-li" >{{item.todo}}</p>
-        </div>
-        </li>
-      </ul>
-      <div class="p-2 self-center">
-            <input type="checkbox" v-model="item.done"/>
-        </div>
-    </div>
+    <v-list :listArray="notDoneTodos" @remove-in="remove(index)"/>
     </div>
     <div class="w-full mt-2">
       <h1 class="bg-cyan-500 text-center rounded-t-lg">Done</h1>
-    <div v-for="(item, index) in doneTodos" :key="item.id" class="flex flex-row w-fit border rounded-lg inline float-left m-1">
-      <button class="p-1 border-r bg-cyan-500 rounded-l-lg" @click.prevent="remove(index)">X</button>
-      <ul class="p-2">
-      <li class="flex flex-col">
-        <div class="flex gap-1">
-        <label for="title-li" class="font-bold">Title:</label>
-         <p id="title-li">{{item.title}} </p>
-        </div>
-        <div class="flex gap-1">
-         <label for="todo-li" class="font-bold">Todo:</label>
-        <p id="todo-li" >{{item.todo}}</p>
-        </div>
-        </li>
-      </ul>
-      <div class="p-2 self-center">
-            <input type="checkbox" v-model="item.done"/>
-        </div>
-    </div>
+    <v-list :listArray="doneTodos" @remove-in="remove(index)"/>
     </div>
     
 </div>
@@ -67,7 +26,12 @@
 
 
 <script>
+import vList from './reusableComponents/vList.vue'
+
 export default {
+  components:{
+    'v-list' : vList
+  },
   name: 'TodoList',
   data(){
     return {
---
+++ src/components/reusableComponents/vList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-for="(item, index) in listArray" :key="item.id" class="flex flex-row border rounded-lg m-1 float-left">
+      <button class="p-1 border-r bg-cyan-500 rounded-l-lg font-semibold" @click="$emit('removeIn',index)">X</button>
+      <ul class="p-2 w-fit">
+      <li class="flex flex-col">
+        <div class="flex gap-1">
+        <label for="title-li" class="font-bold">Title:</label>
+         <p id="title-li">{{item.title}} </p>
+        </div>
+        <div class="flex gap-1">
+         <label for="todo-li" class="font-bold">Todo:</label>
+        <p id="todo-li" >{{item.todo}}</p>
+        </div>
+        </li>
+      </ul>
+      <div class="p-2 self-center">
+            <input type="checkbox" v-model="item.done"/>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+  emits:['removeIn'],
+  name: 'vList',
+  props:{
+    listArray :{
+      type: Array,
+      required: true
+    }
+  }
+  
+}
+</script>
